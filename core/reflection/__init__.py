1aefc0d9570365b3520ef818146e1807bd20e172
Polly Evolution <polly@love.love>
2026-01-16 21:56:06 -0600

    bug

--- core/reflection/__init__.py
+++ core/reflection/__init__.py
@@ -11,12 +11,15 @@ from core.reflection.self_model import (
     generate_self_symbol,
     get_self_symbol,
     refresh_self_symbol,
+    introspect_self,
+    get_context_injection,
 )
 
 from core.reflection.visualize_self_model import (
     build_dependency_graph,
     generate_visualization,
     get_codebase_map,
+    visualize_codebase,
 )
 
 __all__ = [
@@ -25,9 +28,12 @@ __all__ = [
     "generate_self_symbol",
     "get_self_symbol",
     "refresh_self_symbol",
+    "introspect_self",
+    "get_context_injection",
     # Visualization (Story 1.3)
     "build_dependency_graph",
     "generate_visualization",
     "get_codebase_map",
+    "visualize_codebase",
 ]
 
